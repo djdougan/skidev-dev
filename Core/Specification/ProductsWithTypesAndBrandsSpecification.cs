1e3d03519a9d280ed20e664310a743ea71713d93
Douglas Dougan <djdougan@gmail.com>
2021-01-07 11:00:06 -0700

    end of section 7

---
+++ Core/Specification/ProductsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,40 @@
+using Core.Entities;
+
+namespace Core.Specification
+{
+    public class ProductsWithTypesAndBrandsSpecification: BaseSpecification<Product>
+    {
+        public ProductsWithTypesAndBrandsSpecification( ProductSpecParams productParams):base(x=>
+            (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+            (!productParams.BrandId.HasValue|| x.ProductBrandId == productParams.BrandId) && 
+            (!productParams.TypeId.HasValue|| x.ProductTypeId == productParams.TypeId))
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x=>x.ProductBrand);
+            ApplyPaging(productParams.PageSize * (productParams.PageIndex -1), productParams.PageSize);
+
+            AddOrderBy(x=>x.Name);
+            if(!string.IsNullOrEmpty(productParams.Sort)){
+                switch (productParams.Sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p=>p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p=>p.Price);
+                        break;                    
+                    default:
+                        AddOrderBy(p=>p.Name);
+                        break;
+                }
+            }
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int id):base(x=>x.Id == id)
+        {           
+            AddInclude(x => x.ProductType);
+            AddInclude(x=>x.ProductBrand); 
+        }
+        
+    }
+}
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Core.Entities;
+using System.Reflection;
+using System.Linq;
 
 namespace Infrastructure.Data
 {
@@ -11,10 +13,26 @@ namespace Infrastructure.Data
         }
 
         public DbSet<Product> Products{get; set;}
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            if(Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite"){
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties()
+                        .Where(p=>p.PropertyType == typeof(decimal));
+                    foreach (var property in properties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name)
+                        .HasConversion<double>();
+                    }                     
+                }
+            }
         }
 
         
